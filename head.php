fb8315ae64cd6bb319b17000c80e5b5fcb7ee3ba
oleg <bonifazi1985@me.com>
2015-11-01 22:36:20 +0300

    fileupload

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" href="./css/st.css" type="text/css">
     </head>
     <body >
-        <div class="header"><h1>&nbsp;&nbsp;&nbsp;&nbsp;Телефонный справочник</font></h1></div>
+        <div class="header"><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Телефонный справочник</font></h1></div>
 
 
 
